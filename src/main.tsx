f57da426154e8ef0d5449a91bdaa0e88ee48bd80
cryogenized-spec <cryogenized@gmail.com>
2026-08-18 19:31:20 +0200

    feat(v3): mount mobile model tuning panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AppearanceQuickPanel } from './components/AppearanceQuickPanel';
+import { ModelTuningQuickPanel } from './components/ModelTuningQuickPanel';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <AppearanceQuickPanel />
+    <ModelTuningQuickPanel />
   </StrictMode>,
 );
