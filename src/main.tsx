b51364c9a8f1dcffe680b4c88ac7c873169ca828
cryogenized-spec <cryogenized@gmail.com>
2026-08-18 19:59:19 +0200

    feat(v3): expose agent policy editor in mobile UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AppearanceQuickPanel } from './components/AppearanceQuickPanel';
 import { ModelTuningQuickPanel } from './components/ModelTuningQuickPanel';
+import { AgentPolicyPanel } from './components/AgentPolicyPanel';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <AppearanceQuickPanel />
     <ModelTuningQuickPanel />
+    <AgentPolicyPanel />
   </StrictMode>,
 );
