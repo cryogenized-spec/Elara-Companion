55dbf991564c19fefb94e2139ce32291eb92d0ab
cryogenized-spec <cryogenized@gmail.com>
2026-08-27 13:12:58 +0200

    chore: revert accidental main-branch wrapper

--- src/components/LazyCanvasPanel.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const LazyCanvasPanel = React.lazy(async () => {
-  const module = await import('./CanvasPanel');
-  return { default: module.CanvasPanel };
-});
-
-export const CanvasPanel = LazyCanvasPanel;
