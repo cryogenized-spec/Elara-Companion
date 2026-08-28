21d1e170fb01c140918c78bcc5c79b10b78fcc6a
cryogenized-spec <cryogenized@gmail.com>
2026-08-28 03:41:00 +0200

    Stage 2 Pass 8: expose Google capability modules

---
+++ src/components/google/index.ts
@@ -0,0 +1,8 @@
+export { GoogleHub } from './GoogleHub';
+export { GmailCapabilityPanel } from './GmailCapabilityPanel';
+export { CalendarCapabilityPanel } from './CalendarCapabilityPanel';
+export { TasksCapabilityPanel } from './TasksCapabilityPanel';
+export { DriveCapabilityPanel } from './DriveCapabilityPanel';
+export { DocsCapabilityPanel } from './DocsCapabilityPanel';
+export { SheetsCapabilityPanel } from './SheetsCapabilityPanel';
+export { SimpleGoogleCapabilityPanel } from './SimpleGoogleCapabilityPanel';
